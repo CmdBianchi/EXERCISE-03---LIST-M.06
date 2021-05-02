73f84b294a0cc6b47472bd056bda835291384c4b
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-02 15:56:06 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE  03 - LIST M.06/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE__03___LIST_M._06{
+    //------------------------------- START -------------------------------//
+    class Employee{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+        //------------------------- CONSTRUCTORS ------------------------- //
+        public Employee(int id, string name, double salary){
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        //------------------------- CLASS |  MET --------------------------//
+        public void IncreaseSalary(double percentage){
+            Salary += Salary * percentage / 100.0;
+        }
+        public override string ToString(){
+            return Id
+                + ", " + Name + ", " + Salary.ToString("F2");
+        }
+        //-------------------------------- END -------------------------------//
+    }
+}
---
+++ EXERCISE  03 - LIST M.06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace EXERCISE__03___LIST_M._06{
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program{
+        static void Main(string[] args){
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine()); //-----------> ADD o TAMANHO DA LISTA
+            List<Employee> list = new List<Employee>(); //-----------> Chamada do CONSTRUTOR na LISTA
+            for (int i = 1; i <= n; i++){
+                Console.WriteLine("Employee #"+i+ ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+                list.Add(new Employee(id, name, salary)); //-----------> ADD os ELEMENTOS do construtor na LISTA 
+                Console.WriteLine();
+            }
+            Console.WriteLine("Enter the employee Id that will have salary increase : ");
+                int searchId = int.Parse(Console.ReadLine());
+            Employee emp = list.Find(x => x.Id == searchId);
+                if (emp != null){
+                Console.WriteLine("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine());
+                emp.IncreaseSalary(percentage); //-----------> Chamada do METODO DO CONSTRUTOR 
+                }              
+            else{
+                Console.WriteLine("This Id does not existe!");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+                foreach (Employee obj in list){  //-----------> PRINTA a LISTA no console
+                Console.WriteLine(obj);
+                }         
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
